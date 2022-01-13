5d3f6bf120b3556735418969097912e5a57310b1
Ian McConihay <mcconihay.ian92@gmail.com>
2022-01-13 09:22:32 -0600

    Spring boot api calls

--- src/main/java/com/codeup/springblog/controllers/Api.java
+++ src/main/java/com/codeup/springblog/controllers/Api.java
@@ -1,45 +1,45 @@
-package com.codeup.springblog.controllers;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-public class Api {
-
-
-	@RestController
-	@RequestMapping()
-	public String apiCall{
-
-		try {
-
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-		connection.setRequestProperty("accept", "application/json");
-
-		InputStream responseStream = connection.getInputStream();
-
-		ObjectMapper mapper = new ObjectMapper();
-
-		JsonNode root = mapper.readTree(responseStream);
-
-		System.out.println(root.path("fact").asText();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	URL url = new URL("https://api.api-ninjas.com/v1/facts?limit=3");
-
-	public Api() throws MalformedURLException {
-	}
-}
+//package com.codeup.springblog.controllers;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.net.HttpURLConnection;
+//import java.net.MalformedURLException;
+//import java.net.URL;
+//
+//import com.fasterxml.jackson.databind.JsonNode;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.client.RestTemplate;
+//import java.io.InputStream;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//@RestController
+//@RequestMapping("/facts")
+//public class Api {
+//
+//
+//
+//	@GetMapping("/facts")
+//	private String Facts(){
+//		String url = "https://api.api-ninjas.com/v1/facts?limit=3";
+//		RestTemplate restTemplate = new RestTemplate();
+//		String result = restTemplate.getForObject(url, String.class);
+//		return result;
+//	}
+//
+//	URL url = new URL("https://api.api-ninjas.com/v1/facts?limit=3");
+//	HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//connection.setRequestProperty("accept", "application/json");
+//	InputStream responseStream = connection.getInputStream();
+//	ObjectMapper mapper = new ObjectMapper();
+//	JsonNode root = mapper.readTree(responseStream);
+//System.out.println(root.path("fact").asText());
+//}
